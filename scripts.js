55c0e724a624241a1c81dee0e9c4d496bb1ed07b
XForceXS <p1619076@gmail.com>
2025-01-27 12:10:48 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Function to toggle the side menu
+function toggleMenu() {
+    const menu = document.getElementById('side-menu');
+    menu.classList.toggle('active');
+}
+
+// Placeholder for search functionality
+function searchAssignment() {
+    alert('Search functionality to be implemented!');
+}
