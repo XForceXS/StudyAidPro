396700d8a92c18578f0cc7477be2630a2ed15a8a
XForceXS <p1619076@gmail.com>
2025-01-27 13:20:07 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
-// Search functionality
-function searchAssignment() {
-    alert("Search functionality to be implemented");
+// Toggle the visibility of the menu
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
 }
 
-// Menu toggle functionality
-function toggleMenu() {
-    alert("Menu functionality to be implemented");
+// Placeholder function for search
+function searchAssignment() {
+    alert('Search functionality to be implemented');
 }
